cdfd8a0a4341d65ebf4c831c568cb8446fbfd0ac
Sourmpis Christos <sourmpis@node7.pCluster>
2018-05-26 12:43:40 +0200

    big simu data

--- find_mean_score.py
+++ find_mean_score.py
@@ -1,10 +1,20 @@
 import json
 import numpy as np
+import matplotlib.pyplot as plt 
 
-with open("datainh.txt","r") as file:
+
+with open("datainh2.txt","r") as file:
     data = json.load(file)
     score = data["score"]
+    reward = data["reward"]
+    time = data["saving_time"]
 my_max = np.max(score,axis=0)[0]
 score = score[-50:]
 
 print("last 50 matches mean_my_score and my_max:", np.mean(score, axis=0)[0], my_max)
+
+plt.figure()
+plt.plot(time,reward)
+plt.xlabel("time")
+plt.ylabel("reward")
+plt.show()
